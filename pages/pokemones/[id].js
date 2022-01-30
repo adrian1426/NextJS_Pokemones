0ecaeca4f9bed238168c076cad33894ce5bf071a
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-01-29 19:20:09 -0600

    Server Side Rendering - getServerSideProps

--- pages/pokemones/[id].js
+++ pages/pokemones/[id].js
@@ -1,14 +1,27 @@
 import { useRouter } from 'next/router';
 
-const DetallePokemon = () => {
+const DetallePokemon = ({ data }) => {
   const router = useRouter();
   const id = router.query.id;
 
   return (
     <div>
-      <p>pokemon {id}</p>
+      <p>Pokemon: {id} - {data.name}</p>
     </div>
   );
 };
 
 export default DetallePokemon;
+
+export const getServerSideProps = async (props) => {
+  const { params } = props;
+
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`);
+  const data = await response.json();
+
+  return {
+    props: {
+      data
+    }
+  }
+};
